07924bc19ccc106a6f31ca9caf90a4d4f8de6f53
Clarke Brunsdon <clarke@freerunningtechnologies.com>
2013-12-13 10:07:27 -0800

    remove 'java' dependency.
    We can just run jvm_install instead
    
    Change-Id: Ie3710749016871ca8fccca1dae20f7ed1ef8acd4

--- metadata.rb
+++ metadata.rb
@@ -5,5 +5,3 @@ license          "Apache"
 description      "Installs elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.markdown'))
 version          "0.1.0"
-
-depends "java"
